52c6a956f931607adeea385d79da8ed924289d21
sanjayraj122 <babluraj.raj008@gmail.com>
2025-07-05 17:30:58 +0530

    first commit

---
+++ src/main/java/com/aws_lambda/AwsLambdaApplication.java
@@ -0,0 +1,13 @@
+package com.aws_lambda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsLambdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsLambdaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws_lambda/AwsLambdaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws_lambda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsLambdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
